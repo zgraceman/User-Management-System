8070470016fd72ac7c987d995a0d4736f564fd2e
zgraceman <zach.graceman@gmail.com>
2023-07-14 13:39:22 -0500

    Added comprehensive Javadoc and TODOs to UserService class

--- src/main/java/com/example/mySpringApi/service/UserService.java
+++ src/main/java/com/example/mySpringApi/service/UserService.java
@@ -8,12 +8,30 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
+/**
+ * UserService is a service class that provides methods for retrieving User objects
+ * from a predefined list.
+ *
+ * This class is marked with the @Service annotation, making it a candidate
+ * for Spring's component scanning to detect and add to the application context.
+ *
+ * The list of User objects is initialized in the constructor with some dummy data.
+ *
+ * TODO: Integrate with a database or other persistent data store instead of using hardcoded data.
+ * TODO: Add methods to support other operations such as updating and deleting users.
+ */
 @Service
 public class UserService {
 
     private List<User> userList;
 
+    /**
+     * Constructs a new UserService, initializes and populates the userList.
+     *
+     * TODO: Replace the hard-coded user data with data fetched from a persistent data store.
+     */
     public UserService() {
+
         userList = new ArrayList<>();
 
         User user1 = new User(1, "Zach", 23, "zach@gmail.com");
@@ -25,6 +43,15 @@ public class UserService {
         userList.addAll(Arrays.asList(user1, user2, user3, user4, user5));
     }
 
+    /**
+     * Retrieves a User by ID from the list. Returns an Optional<User> that contains
+     * the found User, or is empty if no User with the provided ID is found.
+     *
+     * @param id the ID of the User to retrieve
+     * @return an Optional<User> containing the found User, or empty if not found
+     *
+     * TODO: Replace linear search with a more efficient search method when the user list grows large.
+     */
     public Optional<User> getUser(Integer id) {
 
         System.out.println("getUser Integer id Method");
@@ -40,6 +67,16 @@ public class UserService {
         return optional;
     }
 
+    /**
+     * Retrieves a User by name from the list. Returns an Optional<User> that contains
+     * the found User, or is empty if no User with the provided name is found.
+     *
+     * @param name the name of the User to retrieve
+     * @return an Optional<User> containing the found User, or empty if not found
+     *
+     * TODO: Add handling for case sensitivity and partial matches in user name search.
+     * TODO: Consider whether multiple users could have the same name and how to handle such situations.
+     */
     public Optional<User> getUser(String name) {
 
         System.out.println("getUser String name Method");
@@ -60,7 +97,4 @@ public class UserService {
         }
         return optional;
     }
-
-
-
 }
