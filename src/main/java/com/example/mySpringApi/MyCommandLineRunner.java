123ba59d8d0d0122b40a8f150b574c01fca2fbaa
zgraceman <zach.graceman@gmail.com>
2023-07-16 18:40:27 -0500

    Initialize CommandLineRunner with boilerplate code

---
+++ src/main/java/com/example/mySpringApi/MyCommandLineRunner.java
@@ -0,0 +1,16 @@
+package com.example.mySpringApi;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class MyCommandLineRunner implements CommandLineRunner {
+
+
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+}
