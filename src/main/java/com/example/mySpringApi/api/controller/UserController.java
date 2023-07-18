aa0137b044f1b164ad9f839c480382e50ea19f36
zgraceman <zach.graceman@gmail.com>
2023-07-17 21:35:10 -0500

    Added @PostMapping /addUser endpoint in UserController

--- src/main/java/com/example/mySpringApi/api/controller/UserController.java
+++ src/main/java/com/example/mySpringApi/api/controller/UserController.java
@@ -3,9 +3,7 @@ package com.example.mySpringApi.api.controller;
 import com.example.mySpringApi.model.User;
 import com.example.mySpringApi.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
@@ -38,5 +36,23 @@ public class UserController {
             return (User) user.get();
         }
         return null;
+    }
+
+    /**
+     * Endpoint to add a new user.
+     *
+     * This method is mapped to the "/addUser" endpoint and handles HTTP POST requests.
+     * It takes a User object from the request body, passed by the @RequestBody
+     * annotation. This User object is then passed to the saveUser method of the
+     * UserService, which saves the user to the database. The saved User object,
+     * including any updates made by the database (such as the generated ID for
+     * a new user), is then returned in the HTTP response body.
+     *
+     * @param user A User object that is included in the request body.
+     * @return The saved User object, including any updates made by the database.
+     */
+    @PostMapping("/addUser")
+    public User addUser(@RequestBody User user) {
+        return userService.saveUser(user);
     }
 }
