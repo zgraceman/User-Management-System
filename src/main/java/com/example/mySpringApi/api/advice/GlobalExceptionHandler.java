bfd296b84aa58a1f30e93a0957deedee7ff826dd
zgraceman <zach.graceman@gmail.com>
2023-07-23 17:35:49 -0500

    Renamed GlobalExceptionhandler to UserExceptionHandler. Implemented @ExceptionHandler UserNotFoundException method

--- src/main/java/com/example/mySpringApi/api/advice/GlobalExceptionHandler.java
+++
@@ -1,7 +0,0 @@
-package com.example.mySpringApi.api.advice;
-
-import org.springframework.web.bind.annotation.ControllerAdvice;
-
-@ControllerAdvice
-public class GlobalExceptionHandler {
-}
---
+++ src/main/java/com/example/mySpringApi/api/advice/UserExceptionHandler.java
@@ -0,0 +1,28 @@
+package com.example.mySpringApi.api.advice;
+
+import com.example.mySpringApi.exception.UserException;
+import com.example.mySpringApi.exception.UserNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+// TODO 1. Integrate this exception handling into service layer
+// TODO 2. Test for error: "500 Internal Server Error" again in postman
+// TODO 3. Create and implement ErrorResponse class, instantiate and use in handleUserNotFoundException instead of UserException.
+// TODO 4. Repeat TODO 2.
+@ControllerAdvice
+public class UserExceptionHandler {
+
+    @ExceptionHandler(value = {UserNotFoundException.class})
+    public ResponseEntity<Object> handleUserNotFoundException(UserNotFoundException userNotFoundException) {
+        UserException userException = new UserException(
+                userNotFoundException.getMessage(),
+                userNotFoundException.getCause(),
+                HttpStatus.NOT_FOUND
+        );
+
+        return new ResponseEntity<>(userException, HttpStatus.NOT_FOUND);
+    }
+
+}
