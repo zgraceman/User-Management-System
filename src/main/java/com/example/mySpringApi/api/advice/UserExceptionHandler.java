58719b932b51d1fc74cb502d03544cafd64ec1c0
zgraceman <zach.graceman@gmail.com>
2023-07-25 15:49:22 -0500

    Documentation Added and Updated

--- src/main/java/com/example/mySpringApi/api/advice/UserExceptionHandler.java
+++ src/main/java/com/example/mySpringApi/api/advice/UserExceptionHandler.java
@@ -6,16 +6,33 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+/**
+ * Handles exceptions for the user-related controllers.
+ * It intercepts exceptions thrown within controller methods and formats the error response.
+ *
+ * This class leverages Spring's @ControllerAdvice annotation to provide a centralized exception handling across all
+ * controller classes. The goal is to have a consistent error response structure throughout the application.
+ *
+ * Specific types of exceptions can be handled by adding more methods annotated with @ExceptionHandler.
+ */
 @ControllerAdvice
 public class UserExceptionHandler {
 
+    /**
+     * Handles UserNotFoundExceptions thrown in the application.
+     * It builds an ErrorResponse object using the message and HTTP status from the exception
+     * and returns it in the body of a ResponseEntity.
+     *
+     * @param userNotFoundException The UserNotFoundException that was thrown.
+     * @return A ResponseEntity containing the ErrorResponse and the HTTP status.
+     */
     @ExceptionHandler(value = {UserNotFoundException.class})
-    public ResponseEntity<ErrorResponse> handleUserNotFoundException(UserNotFoundException ex) {
+    public ResponseEntity<ErrorResponse> handleUserNotFoundException(UserNotFoundException userNotFoundException) {
         ErrorResponse errorResponse = new ErrorResponse(
-                ex.getMessage(),
-                ex.getHttpStatus()
+                userNotFoundException.getMessage(),
+                userNotFoundException.getHttpStatus()
         );
 
-        return new ResponseEntity<>(errorResponse, ex.getHttpStatus());
+        return new ResponseEntity<>(errorResponse, userNotFoundException.getHttpStatus());
     }
 }
--- src/main/java/com/example/mySpringApi/exception/UserNotFoundException.java
+++ src/main/java/com/example/mySpringApi/exception/UserNotFoundException.java
@@ -2,8 +2,24 @@ package com.example.mySpringApi.exception;
 
 import org.springframework.http.HttpStatus;
 
+/**
+ * This exception is thrown when a requested user cannot be found.
+ * It is a specific type of {@link UserException}, with a preset message and HTTP status code.
+ *
+ * This class is part of the exception handling framework for the application,
+ * and is used to signal that a requested user-related resource does not exist.
+ *
+ * It extends {@link UserException} to leverage the application's global exception handling
+ * and response formatting.
+ */
 public class UserNotFoundException extends UserException {
 
+    /**
+     * Constructs a new UserNotFoundException with a preset detail message and HTTP status code.
+     *
+     * The detail message is "User not found", indicating that a requested user resource could not be located,
+     * and the HTTP status code is HttpStatus.NOT_FOUND, indicating that the requested resource is not available.
+     */
     public UserNotFoundException() {
         super("User not found", HttpStatus.NOT_FOUND);
     }
