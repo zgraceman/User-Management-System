67c259d861dd5e03cf33955f07b6e03bd85d8037
zgraceman <zach.graceman@gmail.com>
2023-07-25 15:30:10 -0500

    Defined ErrorResponse class

--- src/main/java/com/example/mySpringApi/api/response/ErrorResponse.java
+++ src/main/java/com/example/mySpringApi/api/response/ErrorResponse.java
@@ -1,4 +1,32 @@
 package com.example.mySpringApi.api.response;
 
+import org.springframework.http.HttpStatus;
+
+/**
+ * Represents an HTTP error response.
+ * This class is used to send an error message and status back to the client when an exception occurs.
+ */
 public class ErrorResponse {
+
+    private final String message;
+    private final HttpStatus httpStatus;
+
+    /**
+     * Constructs a new ErrorResponse with specified detail message and HTTP status code.
+     *
+     * @param message The detail message (which is saved for later retrieval by the getMessage() method).
+     * @param httpStatus The HTTP status code that is suitable for this error (which is saved for later retrieval by the getHttpStatus() method).
+     */
+    public ErrorResponse(String message, HttpStatus httpStatus) {
+        this.message = message;
+        this.httpStatus = httpStatus;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public HttpStatus getHttpStatus() {
+        return httpStatus;
+    }
 }
