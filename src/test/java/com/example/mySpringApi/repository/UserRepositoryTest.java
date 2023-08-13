316f287df4e5f65eb50cd5fd36da979c0a77195f
ZachGraceman <zach.graceman@gmail.com>
2023-08-13 18:05:16 -0500

    Created test cases in UserRepositoryTests
    Added a test class 'UserRepositoryTest' to validate repository operations
    for the User entity. The tests cover finding users by name and email, and
    saving a new user to the database

--- src/test/java/com/example/mySpringApi/repository/UserRepositoryTest.java
+++ src/test/java/com/example/mySpringApi/repository/UserRepositoryTest.java
@@ -1,4 +1,65 @@
 package com.example.mySpringApi.repository;
 
+import com.example.mySpringApi.model.User;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
 public class UserRepositoryTest {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private TestEntityManager entityManager; // For setting up database state
+
+    private User testUser;
+
+    @BeforeEach
+    public void setUp() {
+        testUser = new User("TestUser", 24, "TestUser@gmail.com");
+
+        entityManager.persist(testUser);
+        entityManager.flush();
+    }
+
+    @AfterEach
+    public void cleanUp() {
+        userRepository.deleteAll(); // This will clear the h2 database after each test
+    }
+
+    @Test
+    public void whenValidName_thenUserShouldBeFound() {
+        Optional<User> found = userRepository.findByName(testUser.getName());
+
+        assertThat(found).isPresent();
+        assertThat(found.get().getName()).isEqualTo(testUser.getName());
+    }
+
+    @Test
+    public void whenValidEmail_thenUserShouldBeFound() {
+        Optional<User> found = userRepository.findByEmail(testUser.getEmail());
+
+        assertThat(found).isPresent();
+        assertThat(found.get().getEmail()).isEqualTo(testUser.getEmail());
+    }
+
+    @Test
+    public void whenSavingUser_thenUserShouldBePersisted() {
+        User newUser = new User("AliceTest", 60, "aliceTest@example.com");
+
+        User savedUser = userRepository.save(newUser);
+        Optional<User> found = userRepository.findById(savedUser.getId());
+
+        assertThat(found).isPresent();
+        assertThat(found.get().getName()).isEqualTo(newUser.getName());
+    }
 }
