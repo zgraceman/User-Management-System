a26345aba3e27336cddaa96492116d18e6441455
zgraceman <zach.graceman@gmail.com>
2023-09-18 19:44:40 -0500

    Implemented and experimenting with Swagger UI

---
+++ src/main/java/com/example/mySpringApi/config/OpenApiConfig.java
@@ -0,0 +1,37 @@
+package com.example.mySpringApi.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition(
+        info = @Info(
+                contact = @Contact(
+                        name = "Zach",
+                        email = "zach@example.com",
+                        url = "https://google.com"
+                ),
+                description = "OpenApi documentation",
+                title = "OpenApi specification - Zach",
+                version = "1.0",
+                license = @License(
+                        name = "Licence name",
+                        url = "https://some-url.com"
+                ),
+                termsOfService = "Terms of service"
+        ),
+        servers = {
+                @Server(
+                        description = "Local ENV",
+                        url = "http://localhost:8080"
+                ),
+                @Server(
+                        description = "PROD ENV",
+                        url = "http://localhost:8080"
+                ),
+        }
+)
+public class OpenApiConfig {
+}
