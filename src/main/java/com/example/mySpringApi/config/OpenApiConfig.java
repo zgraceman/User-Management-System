eaafb77f0fb58bf0cdca5e66afd71d34827b5137
zgraceman <zach.graceman@gmail.com>
2023-09-19 20:50:32 -0500

    Added Authorization button to Swagger UI

--- src/main/java/com/example/mySpringApi/config/OpenApiConfig.java
+++ src/main/java/com/example/mySpringApi/config/OpenApiConfig.java
@@ -1,9 +1,12 @@
 package com.example.mySpringApi.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.annotations.servers.Server;
 
 @OpenAPIDefinition(
@@ -32,6 +35,14 @@ import io.swagger.v3.oas.annotations.servers.Server;
                         url = "http://localhost:8080"
                 ),
         }
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        description = "JWT auth authentication",
+        scheme = "bearer",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        in = SecuritySchemeIn.HEADER
 )
 public class OpenApiConfig {
 }
