5be7ce9b79944c9f230eb61a463e37524c7f1759
zgraceman <zach.graceman@gmail.com>
2023-10-26 21:02:49 -0500

    feat(security): implement basic security configuration and add welcome page
    - Added a new Spring Security configuration to secure the application.
    - Configured basic HTTP security to permit all requests to the root path, CSS, and JS files.
    - Implemented HTTP basic authentication for other endpoints.
    - Added a new index.html file to serve as a welcome page for users.

---
+++ src/main/java/com/example/mySpringApi/security/ApplicationSecurityConfig.java
@@ -0,0 +1,23 @@
+package com.example.mySpringApi.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+
+@Configuration
+@EnableWebSecurity
+public class ApplicationSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authz -> authz
+                        .requestMatchers("/","index","/css/*","/js/*").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .httpBasic();
+        return http.build();
+    }
+}
