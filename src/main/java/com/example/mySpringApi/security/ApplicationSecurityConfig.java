dfac65052f15ae67b176ec23facf268c30d15a45
zgraceman <zach.graceman@gmail.com>
2023-10-27 20:39:50 -0500

    Integrate Custom UserDetailsService in Security Config
    - Autowired MyUserDetailsService for user authentication.
    - Configured BCryptPasswordEncoder as the password encoder.
    - Implemented DaoAuthenticationProvider to use custom UserDetailsService and password encoder.
    - Updated SecurityFilterChain to handle user authentication and authorization.

--- src/main/java/com/example/mySpringApi/security/ApplicationSecurityConfig.java
+++ src/main/java/com/example/mySpringApi/security/ApplicationSecurityConfig.java
@@ -1,8 +1,12 @@
 package com.example.mySpringApi.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 
@@ -10,6 +14,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 @EnableWebSecurity
 public class ApplicationSecurityConfig {
 
+    @Autowired
+    private MyUserDetailsService userDetailsService;
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
@@ -20,4 +26,19 @@ public class ApplicationSecurityConfig {
                 .httpBasic();
         return http.build();
     }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider daoAuthenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(passwordEncoder());
+        provider.setUserDetailsService(userDetailsService);
+        return provider;
+    }
+
+
 }
---
+++ src/main/java/com/example/mySpringApi/security/MyUserDetailsService.java
@@ -0,0 +1,25 @@
+package com.example.mySpringApi.security;
+
+import com.example.mySpringApi.model.User;
+import com.example.mySpringApi.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + email));
+
+        return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), new ArrayList<>());
+    }
+}
