7effe99ef047c5ad66ccecf74a639f3234e5500e
zgraceman <zach.graceman@gmail.com>
2023-11-10 20:41:39 -0600

    Resolve Circular Dependency Issue in Security Configuration
    - Separate PasswordEncoder into its own configuration class (PasswordEncoderConfig).
    - Refactor SecurityConfig to use constructor injection for CustomUserDetailsService and PasswordEncoder.
    - Adjust application context to prevent circular dependencies between security-related beans.
    - Ensure independent instantiation of PasswordEncoder to enhance configuration clarity and resolve the BeanCurrentlyInCreationException issue.

--- src/main/java/com/example/mySpringApi/Security/SecurityConfig.java
+++ src/main/java/com/example/mySpringApi/Security/SecurityConfig.java
@@ -4,6 +4,7 @@ import com.example.mySpringApi.service.CustomUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -20,8 +21,13 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class SecurityConfig {
 
-    @Autowired
-    private CustomUserDetailsService customUserDetailsService;
+    private final CustomUserDetailsService customUserDetailsService;
+    private final PasswordEncoder passwordEncoder;
+
+    public SecurityConfig(CustomUserDetailsService customUserDetailsService, PasswordEncoder passwordEncoder) {
+        this.customUserDetailsService = customUserDetailsService;
+        this.passwordEncoder = passwordEncoder;
+    }
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
@@ -37,11 +43,6 @@ public class SecurityConfig {
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(customUserDetailsService)
-                .passwordEncoder(passwordEncoder());
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+                .passwordEncoder(passwordEncoder);
     }
 }
