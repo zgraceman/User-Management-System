c4a7f20898eac1c9d07756ac583df3badcb26410
zgraceman <zach.graceman@gmail.com>
2023-11-21 15:26:14 -0600

    Document Role Entity and RoleRepository Interface
    - Added detailed documentation to the Role entity class, clarifying its role in the system as a representation of user roles for RBAC.
    - Documented the fields and annotations within the Role class, including explanations for the entity's id and name.
    - Provided comprehensive documentation for the RoleRepository interface, outlining its purpose in managing role data.
    - Described the functionality of the findByName and findByNameIn methods in RoleRepository, explaining their usage in role queries.

--- src/main/java/com/example/mySpringApi/model/Role.java
+++ src/main/java/com/example/mySpringApi/model/Role.java
@@ -5,6 +5,16 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+/**
+ * The Role entity represents a role or authority that can be assigned to a user in the application.
+ * It maps to the "role" table in the database and is used to implement role-based access control (RBAC).
+ *
+ * Annotations:
+ * - @Entity: Specifies that this class is an entity and is mapped to a database table.
+ * - @Getter, @Setter: Lombok annotations to automatically generate getter and setter methods for the fields.
+ * - @NoArgsConstructor: Lombok annotation to generate a no-argument constructor.
+ * - @Table(name = "role"): Specifies the name of the table in the database for this entity.
+ */
 @Entity
 @Getter
 @Setter
--- src/main/java/com/example/mySpringApi/repository/RoleRepository.java
+++ src/main/java/com/example/mySpringApi/repository/RoleRepository.java
@@ -6,7 +6,30 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 import java.util.Set;
 
+/**
+ * RoleRepository is a JPA repository for the Role entity.
+ * It provides a way to perform various operations on the role data in the database, such as
+ * querying roles by name and finding roles within a set of names.
+ *
+ * This interface extends JpaRepository, which provides standard CRUD operations and JPA query capabilities.
+ */
 public interface RoleRepository extends JpaRepository<Role, Long> {
+
+    /**
+     * Finds a role by its name.
+     * This method is used to fetch a specific role from the database based on its name.
+     *
+     * @param name The name of the role to find.
+     * @return An Optional containing the Role if found, or empty if not.
+     */
     Optional<Role> findByName(String name);
+    
+    /**
+     * Finds roles by a set of names.
+     * This method is used to fetch multiple roles from the database based on a set of role names.
+     *
+     * @param names The set of role names to find.
+     * @return A set of Roles corresponding to the given names.
+     */
     Set<Role> findByNameIn(Set<String> names);
 }
