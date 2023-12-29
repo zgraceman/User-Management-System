00d74b03398188e767fa0de10cd3839b9584cfdf
zgraceman <zach.graceman@gmail.com>
2023-12-29 12:41:33 -0600

    Fixed Role Deletion Issue on User Removal
    - Removed CascadeType.ALL from the User-Role relationship mapping.
    - Ensures that deleting a user does not inadvertently delete associated roles.

--- src/main/java/com/example/mySpringApi/model/User.java
+++ src/main/java/com/example/mySpringApi/model/User.java
@@ -44,7 +44,7 @@ public class User {
 
     private String password;
 
-    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(
             name = "user_roles",
             joinColumns = @JoinColumn(name = "user_id"),
